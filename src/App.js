3386fbd1446d7d41a9da28ec7aef457a6f23be6f
Victoria Akpan <veedahv55@gmail.com>
2022-05-18 22:26:53 +0100

    ui change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import { useEffect, useState } from 'react';
 import './App.css';
 import ResponseCard from './components/ResponseCard';
@@ -6,25 +5,7 @@ import ResponseCard from './components/ResponseCard';
 function App() {
   const [fetching, setFetching] = useState(false);
   const [prompt, setPrompt] = useState('');
-  const [responses, setResponses] = useState([
-    // {
-    //   prompt: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    //   response: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non architecto expedita iste, repellendus quas quam quis eius asperiores illum provident amet accusamus odio obcaecati cupiditate optio officia rerum ex autem molestiae quo facere veritatis labore consequatur! Similique rerum labore id atque? Quod aliquid ipsam aperiam ducimus voluptates officiis neque et!"
-    // },
-    // {
-    //   prompt: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    //   response: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non architecto expedita iste, repellendus quas quam quis eius asperiores illum provident amet accusamus odio obcaecati cupiditate optio officia rerum ex autem molestiae quo facere veritatis labore consequatur! Similique rerum labore id atque? Quod aliquid ipsam aperiam ducimus voluptates officiis neque et!"
-    // },
-    // {
-    //   prompt: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    //   response: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non architecto expedita iste, repellendus quas quam quis eius asperiores illum provident amet accusamus odio obcaecati cupiditate optio officia rerum ex autem molestiae quo facere veritatis labore consequatur! Similique rerum labore id atque? Quod aliquid ipsam aperiam ducimus voluptates officiis neque et!"
-    // },
-    // {
-    //   prompt: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    //   response: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non architecto expedita iste, repellendus quas quam quis eius asperiores illum provident amet accusamus odio obcaecati cupiditate optio officia rerum ex autem molestiae quo facere veritatis labore consequatur! Similique rerum labore id atque? Quod aliquid ipsam aperiam ducimus voluptates officiis neque et!"
-    // }
-  ]);
-  // prompt: "Write a poem about a dog wearing skis",
+  const [responses, setResponses] = useState([]);
   const data = {
     prompt: prompt,
     temperature: 0.5,
@@ -33,27 +14,15 @@ function App() {
     frequency_penalty: 0.0,
     presence_penalty: 0.0,
   }
-
-
   useEffect(() => {
     if (localStorage.getItem("promptResponses")) {
-      console.log(JSON.parse(localStorage.getItem("promptResponses")));
       setResponses(JSON.parse(localStorage.getItem("promptResponses")));
       localStorage.setItem("promptResponses", JSON.stringify(JSON.parse(localStorage.getItem("promptResponses"))));
-
     }
   }, [])
-
-
-
-
   const fetchPrompt = (e) => {
     e.preventDefault();
     setFetching(true);
-    
-    console.log(prompt);
-    console.log(data);
-    // console.log("fetching");
     fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
       method: "POST",
       headers: {
@@ -62,31 +31,20 @@ function App() {
       },
       body: JSON.stringify(data),
     })
-    .then(res => { return res.json() })
-    .then(data => {
-      // console.log(data);
-      //   console.log(data.choices);
-      //   console.log(data.choices[0]);
-      //   console.log(data.choices[0].text);
-      let newResponse = {
+      .then(res => res.json())
+      .then(data => {
+        let newResponse = {
           prompt: prompt,
           response: data.choices[0].text
         }
         setResponses(prevState =>
           [...prevState, newResponse]
-          )
-          localStorage.setItem("promptResponses", JSON.stringify([...responses, newResponse]));
-          // localStorage.setItem("promptResponses", JSON.stringify([responses]));
-          console.log("done");
-          setPrompt(" ")
-        });
-        console.log(responses)
-        setFetching(false);
-        // .then(data => console.log(data));
-    
-    // Authorization: `Bearer ${process.env.OPENAI_SECRET}`,
+        )
+        localStorage.setItem("promptResponses", JSON.stringify([...responses, newResponse]));
+        setPrompt("");
+      });
+    setFetching(false);
   }
-
   const clearResponses = () => {
     localStorage.removeItem("promptResponses");
     setResponses([])
@@ -101,19 +59,17 @@ function App() {
                 Fun with API
               </h1>
               <div className="flex items-center justify-center gap-5">
-              <p className='text-xl'>
-                Ask me anything
-              </p>
-                {/* <button className="rounded-md bg-red-200 text-xl font-medium px-5 md:w-44 lg:w-60" type='button' onClick={() => clearResponses()}>Clear all</button> */}
-                <button className="rounded-md text-red-500 text-lg font-medium underline" type='button' onClick={() => clearResponses()}>Clear all</button>
-                </div>
+                <p className='text-xl'>
+                  Ask me anything
+                </p>
+                {fetching && <button className="rounded-md text-red-500 text-lg font-medium underline" type='button' onClick={() => clearResponses()}>Clear all</button>}
+              </div>
             </div>
             <div className="responses-container flex-auto w-full">
               <div className="h-[70vh] flex overflow-y-auto p-3 w-full">
                 <div className="w-full">
                   {
                     responses.map((response, i) => {
-                      // console.log(response)
                       return (
                         <ResponseCard response={response} key={i} />
                       )
@@ -124,13 +80,17 @@ function App() {
             </div>
             <div className="h-fit">
               <form className="form flex items-stretch gap-2 py-3" onSubmit={(e) => fetchPrompt(e)}>
-                <textarea 
-                rows="2" 
-                placeholder='Enter Prompt...' 
-                className="w-full border border-gray-300 rounded-md px-5 py-2.5 focus:shadow outline-none" 
-                      value={prompt} onChange={(e) => setPrompt(e.target.value)}>
+                <textarea
+                  rows="2"
+                  placeholder='Enter Prompt...'
+                  className="w-full border border-gray-300 rounded-md px-5 py-2.5 focus:shadow outline-none"
+                  value={prompt} onChange={(e) => setPrompt(e.target.value)}>
                 </textarea>
-                <button className="rounded-md bg-green-200 text-xl font-medium px-5 md:w-44 lg:w-60" type='submit' disabled={fetching}>Submit</button>
+                {
+                  fetching ?
+                    <button className="rounded-md bg-green-200 text-lg font-medium px-5 md:w-44 lg:w-60" disabled>loading</button> :
+                    <button className="rounded-md bg-green-200 text-xl font-medium px-5 md:w-44 lg:w-60" type='submit'>Submit</button>
+                }
               </form>
             </div>
           </div>
--- src/components/ResponseCard.js
+++ src/components/ResponseCard.js
@@ -2,36 +2,6 @@
 function ResponseCard({ response }) {
     return (
         <div className="w-full">
-            {/* <div className="w-full">
-                <div className="flex gap-1 items-start max-w-[95%] my-3">
-                    <div className="rounded-full bg-gray-300 w-12 h-12 flex items-center justify-center">
-                        <h3 className="font-semibold text-3xl">
-                            P
-                        </h3>
-                    </div>
-                    <div className="rounded-md bg-gray-300 p-3">
-                        <div className="">
-                            <p className="text-lg">
-                                {response.prompt}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex gap-1 items-start max-w-[95%] ml-auto my-3">
-                    <div className="rounded-md bg-slate-100 p-3 md:p-5 w-full">
-                        <div className="">
-                            <p className="text-lg">
-                                {response.response}
-                            </p>
-                        </div>
-                    </div>
-                    <div className="rounded-full bg-gray-300 w-12 h-12 flex items-center justify-center">
-                        <h3 className="font-semibold text-3xl">
-                            R
-                        </h3>
-                    </div>
-                </div>
-            </div> */}
             <div className="w-full">
                 <div className="flex justify-end max-w-[90%] ml-auto my-3">
                     <div className="rounded-md bg-slate-100 p-3 md:p-5">
